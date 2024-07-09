d1150ea7124246e25ff3e3ef8287ead60e042002
Rytham1 <Rythamdawar30@gmail.com>
2024-07-08 18:46:29 -0700

    Completed w/ minor axios bug

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
 import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import Axios from "axios";
+
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
@@ -37,8 +39,27 @@ class AddTodo extends Component {
   // this.props.addTodo(this.state) passes the current state (or user input and current date/time) into the addTodo function defined
   // in the Home.js file which then adds the input into the list.
   handleSubmit = (event) => {
+
+    if(this.state.content.trim()) {
+      const jsonObject = {
+        id: this.state.id,
+        task: this.state.content.trim(),
+        currentDate: this.state.date,
+        dueDate: this.state.duedate
+      };
+      Axios({
+        method: "POST",
+        url: "http://localhost:3001/add/item",
+        data: {jsonObject},
+        headers: {
+           "Content-Type": "application/json"
+      }
+    }).then(res => {
+        console.log(res.data.message);
+    });
+
     event.preventDefault();
-    if (this.state.content.trim()) {
+    
       this.props.addTodo(this.state);
       this.setState({
         content: "",
